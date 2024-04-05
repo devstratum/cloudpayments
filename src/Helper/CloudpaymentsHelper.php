4e32f8c0bd6a3915b184842f9fe86f4ab04e8c59
Sergey Osipov <47226965+devstratum@users.noreply.github.com>
2024-04-05 19:57:35 +0300

    preparing...

---
+++ src/Helper/CloudpaymentsHelper.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package         Cloudpayments
+ * @version         1.0.0
+ * @author          Sergey Osipov <info@devstratum.ru>
+ * @website         https://devstratum.ru
+ * @copyright       Copyright (c) 2024 Sergey Osipov. All Rights Reserved
+ * @license         GNU General Public License v2.0
+ * @report          https://github.com/devstratum/cloudpayments/issues
+ */
+
+namespace Devstratum\Module\Cloudpayments\Site\Helper;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Response\JsonResponse;
+use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\Input\Json;
+use Joomla\CMS\Language\Text;
+
+/**
+ * Helper for mod_cloudpayments
+ *
+ * @since   1.0.0
+ */
+class CloudpaymentsHelper
+{
+    /**
+     * Method set Response
+     *
+     * @param   array   $response
+     * @param   array   $message
+     * @throws
+     * @since   1.0.0
+     */
+    public static function setResponse($response, $message)
+    {
+        $app = Factory::getApplication();
+        header('Content-Type: application/json');
+        echo new JsonResponse($response, $message);
+        $app->close();
+    }
+}
